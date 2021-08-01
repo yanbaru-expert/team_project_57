90f67d51df488edb5c614622936c73118573aba5
久野泰行 <>
2021-08-01 09:16:25 +0900

    モデル・テーブルの作成

---
+++ db/migrate/20210731073329_create_texts.rb
@@ -0,0 +1,10 @@
+class CreateTexts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :texts do |t|
+      t.integer :genre, null: false, default: 0
+      t.string :title, null: false
+      t.text :contnt, null: false
+      t.timestamps
+    end
+  end
+end
