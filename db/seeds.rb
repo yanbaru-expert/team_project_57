898f2888d02ca76a30bb3dfc9d82161f0da483ba
idakazuki <kazuki.fykgu@gmail.com>
2021-08-05 07:33:56 +0900

    unicodeで記述されていた箇所を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,5 @@ password = "password"
 # テストユーザーが存在しないときだけ作成
 User.find_or_create_by!(email: email) do |user|
   user.password = password
-  puts "\u30E6\u30FC\u30B6\u30FC\u306E\u521D\u671F\u30C7\u30FC\u30BF\u30A4\u30F3\u30DD\u30FC\u30C8\u306B\u6210\u529F\u3057\u307E\u3057\u305F\u3002"
+  puts "ユーザーの初期データインポートに成功しました。"
 end
