858eed90d8da92b0a8ec3924a023595df8720a41
Akustino0310 <niggy2014@yahoo.co.jp>
2021-08-12 23:16:07 +0900

    ゲストログイン処理の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   resources :texts, only: [:index, :show]
   resources :movies, only: [:index]
   root "texts#index"
