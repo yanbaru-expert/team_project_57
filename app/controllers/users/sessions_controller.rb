78bc01410708d63bcc5003d05f2fcdca45faae4c
boy-com777 <hiroyukiogasawara@ogasawarahiroyukinoMacBook-Pro.local>
2021-08-15 17:45:29 +0900

    movie.rbに「RAILS_GENRE_LIST」を定義,index.html.erbに一覧を実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,10 @@
-class Users::SessionsController < Devise::SessionsController
-  def guest_sign_in
-    # ゲストアカウントでログイン
-    sign_in User.guest
-    # トップページへリダイレクト
-    redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
+module Users
+  class SessionsController < Devise::SessionsController
+    def guest_sign_in
+      # ゲストアカウントでログイン
+      sign_in User.guest
+      # トップページへリダイレクト
+      redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
+    end
   end
 end
