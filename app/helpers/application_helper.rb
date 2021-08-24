1c8e0a57ad9222edc050a8c45b615cfb030b1f12
YasuyukiKuno <kunoyasuyuki@yahoo.co.jp>
2021-08-25 08:05:39 +0900

    不要な表示の修正とPHPテキスト教材がただしく表示されるように修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,14 @@ module ApplicationHelper
     end
   end
 
+  def text_genre
+    if params[:genre] == "php"
+      "PHP テキスト教材"
+    else
+      "Ruby/Rails テキスト教材"
+    end
+  end
+
   def movie_genre
     if params[:genre] == "php"
       "PHP 動画"
