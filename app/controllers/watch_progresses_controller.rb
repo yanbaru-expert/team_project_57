352def96a9a5d718563c79b71faf21a5eb2ceb7f
idakazuki <kazuki.fykgu@gmail.com>
2021-08-26 15:07:28 +0900

    視聴済み機能を実装

--- app/controllers/watch_progresses_controller.rb
+++ app/controllers/watch_progresses_controller.rb
@@ -1,11 +1,15 @@
 class WatchProgressesController < ApplicationController
   def create
-    @movie = Movie.find(params[:movie_id])
-    current_user.watch_progresses.create!(movie_id: @movie.id)
+    # @movie = Movie.find(params[:movie_id])
+    # current_user.watch_progresses.create!(movie_id: @movie.id)
+    current_user.watch_progresses.create!(movie_id: params[:movie_id])
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    @movie = Movie.find(params[:movie_id])
-    current_user.watch_progresses.find_by(movie_id: @movie.id).destroy!
+    # @movie = Movie.find(params[:movie_id])
+    # current_user.watch_progresses.find_by(movie_id: @movie.id).destroy!
+    current_user.watch_progresses.find_by(movie_id: params[:movie_id]).destroy!
+    redirect_back(fallback_location: root_path)
   end
 end
